b0e6ff75c7945c21fedf4426de139f960cf64719
Vikas Panwar <cs.panwar@gmail.com>
2021-12-13 22:22:51 +0530

    Email Validation on Array of Emails

--- 02_InterfacesCollections/src/simplilearn/java/EmailValidation.java
+++ 02_InterfacesCollections/src/simplilearn/java/EmailValidation.java
@@ -1,28 +1,35 @@
 package simplilearn.java;
 
-import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EmailValidation {
 
 	public static void main(String[] args) {
-		String pattern = "^[A-Za-z0-9+_.-]+@(.+)$";
-		Pattern p = Pattern.compile(pattern);
+		ArrayList<String> emails = new ArrayList<>();
+		emails.add("abc@domain.co.in");
+		emails.add("abc@domain.com");
+		emails.add("ab-ax#@domain.co.in");
+		emails.add("ab_cd@domain.com");
+		emails.add("abCDxyz@domaincom");
 
-		boolean Invalid = true;
-		while (Invalid) {
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Please enter your email_Id?");
+		// Invalid list
+		emails.add("@yahoo.com");
+		emails.add("12abc#domain.com");
 
-			String email = sc.nextLine();
-			Matcher m = p.matcher(email);
-			if (m.matches()) {
-				System.out.println("Great!! That is a valid email");
-				Invalid = false;
-			} else {
-				System.out.println("Please provide a valid email address");
+		String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
+		Pattern pattern = Pattern.compile(regex);
+
+		// Iterate emails array list
+		for (String email : emails) {
+			// Create instance of matcher
+			Matcher matcher = pattern.matcher(email);
+			if (matcher.matches()) {
+				System.out.println(email + " is a valid email address\n");
+				continue;
 			}
+			System.out.println(email + " is an Invalid email address\n");
 		}
 	}
 }
